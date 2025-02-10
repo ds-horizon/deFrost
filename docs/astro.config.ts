0389b6f9ed35d073568866a901d0218354c11376
sarthak-d11 <sarthak.agarwal@dream11.com>
2025-02-10 13:31:32 +0530

    feat: docs added

---
+++ docs/astro.config.ts
@@ -0,0 +1,66 @@
+// @ts-check
+import starlight from '@astrojs/starlight';
+
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+
+import { defineConfig } from 'astro/config';
+
+const SITE = 'https://marco.dreamsportslabs.com';
+
+// https://astro.build/config
+export default defineConfig({
+  site: SITE,
+  output: 'static',
+  integrations: [
+    starlight({
+      title: 'De-frost',
+      pagination: true,
+      titleDelimiter: '/',
+      description: 'Track and optimize your android app’s frozen frames with ease.',
+      logo: {
+        dark: './src/assets/logo-dark.svg',
+        light: './src/assets/logo-light.svg',
+        alt: 'Defrost Logo',
+        replacesTitle: true,
+      },
+      social: {
+        github: 'https://github.com/dream-sports-labs/marco',
+        discord: 'https://discord.gg/RBDPaN2V',
+      },
+      sidebar: [
+        {
+          label: 'Project',
+          autogenerate: { directory: 'project' },
+        },
+        {
+          label: 'Dashboard',
+          items: [
+            'dashboard/overview',
+            'dashboard/android-frames',
+            'dashboard/react-commits',
+            'dashboard/how-to-use',
+          ],
+        },
+        {
+          label: 'CLI',
+          items: [
+            'cli/create-build',
+            'cli/record',
+            'cli/visualization',
+          ],
+        },
+        {
+          label: 'Guides',
+          autogenerate: { directory: 'guides' },
+        },
+      ],
+      customCss: ['./src/tailwind.css', '@fontsource-variable/inter'],
+      components: {
+        Head: './src/overrides/head.astro',
+      },
+    }),
+    tailwind({ applyBaseStyles: false }),
+    react(),
+  ],
+});
